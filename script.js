0c79e4c9d5864d3865fe9141fbc52f23e3d535ac
Jordin Gutierrez <sjgutierrez2016@gmail.com>
2024-06-04 13:33:10 -0500

    Tarea 1 siglas matematicas

---
+++ script.js
@@ -0,0 +1,22 @@
+let num1,num2;
+let suma = 0;
+let resta = 0;
+let multiplicacion = 0;
+let division = 0;
+
+num1= Number(prompt(`ingrese numer 5`));
+num2= Number(prompt(`ingrese numero 7`));
+
+suma = num1 + num2 
+resta = num1 - num2
+multiplicacion = num1 * num2 
+division = num1 / num2
+
+console.log(`la suma de ${num1} + ${num2} es ${suma}`);
+
+ console.log(`la resta de ${num1} - ${num2} es ${resta}`);
+
+ console.log(`la multiplicacion de ${num1} * ${num2} es ${multiplicacion}`);
+ 
+ console.log(`la division de ${num1} / ${num2} es ${division}`);
+
